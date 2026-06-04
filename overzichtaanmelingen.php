3bbfd90c8ef4368af231e1f92a7b192f5131aac0
runnengames2 <runnengames@gmail.com>
2026-06-04 10:33:08 +0200

    Nav menu link fix

--- overzichtaanmelingen.php
+++ overzichtaanmelingen.php
@@ -33,9 +33,9 @@ $result = $conn->query($sql);
 
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="bestel.php">overzicht tickets</a></li>
-                <li class="nav-item"><a class="nav-link" href="aanmelddeelnemers.php">overzicht aanmeldingen</a></li>
-                <li class="nav-item"><a class="nav-link" href="login.php">Logout</a></li>
+                <li class="nav-item"><a class="nav-link" href="overzichttickets.php">overzicht tickets</a></li>
+                <li class="nav-item"><a class="nav-link active" href="overzichtaanmeldingen.php">overzicht aanmeldingen</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Logout</a></li>
             </ul>
         </div>
 
